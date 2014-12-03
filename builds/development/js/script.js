ebb17904d8b587515098ce150d7e35fdef7aa6b2
Ray Villalobos <ray@planetoftheweb.com>
2014-12-03 14:30:23 -0500

    02_03 Manipulating Arrays

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -6,23 +6,25 @@ var output = [], update;
 var	info = {
 	"full_name" : "Ray Villalobos",
 	"title" : "Staff Author",
-	"links" : {
-    "blog"     : "http://iviewsource.com",
-    "facebook" : "http://facebook.com/iviewsource",
-		"youtube"  : "http://www.youtube.com/planetoftheweb",
-		"podcast"  : "http://feeds.feedburner.com/authoredcontent",
-		"twitter"  : "http://twitter.com/planetoftheweb" 
-	}
+	"links" : [
+			{ "blog"     : "http://iviewsource.com" },
+			{ "facebook" : "http://facebook.com/iviewsource" },
+			{ "podcast"  : "http://feeds.feedburner.com/authoredcontent" },
+			{ "twitter"  : "http://twitter.com/planetoftheweb" },
+			{ "youtube"  : "http://www.youtube.com/planetoftheweb" }
+		]
 };
 
-for (var key in	info.links) {
-	if (info.links.hasOwnProperty(key)) {
-		output.push('<li id="' + key + '">'); 
-		output.push('<a href="' + info.links[key]); 
-		output.push('">' + key + '</a>');
-		output.push('</li>');
-	}
-}
+for (var i = 0; i <= info.links.length-1; i++) {
+	for (var key in info.links[i]) {
+		if (info.links[i].hasOwnProperty(key)) {
+			output.push('<li id="' + key + '">'); 
+			output.push('<a href="' + info.links[i][key]); 
+			output.push('">' + key + '</a>');
+			output.push('</li>');
+    } // make sure property exists
+	} // go through each object
+} // go through each array element
 
 update = document.querySelector('.links ul');
 update.innerHTML = output.join('');
