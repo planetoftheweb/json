a17880cb471a05e8e2429467deb2946fefa8563e
Ray Villalobos <ray@planetoftheweb.com>
2014-12-03 18:51:09 -0500

    03_01 AJAX call

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,31 +2,36 @@
 
 "use strict";
 
-var output = [], update;
-var	info = {
-	"full_name" : "Ray Villalobos",
-	"title" : "Staff Author",
-	"links" : [
-			{ "blog"     : "http://iviewsource.com" },
-			{ "facebook" : "http://facebook.com/iviewsource" },
-			{ "podcast"  : "http://feeds.feedburner.com/authoredcontent" },
-			{ "twitter"  : "http://twitter.com/planetoftheweb" },
-			{ "youtube"  : "http://www.youtube.com/planetoftheweb" }
-		]
-};
+var output = [], update, info;
+var request;
 
-for (var i = 0; i <= info.links.length-1; i++) {
-	for (var key in info.links[i]) {
-		if (info.links[i].hasOwnProperty(key)) {
-			output.push('<li id="' + key + '">'); 
-			output.push('<a href="' + info.links[i][key]); 
-			output.push('">' + key + '</a>');
-			output.push('</li>');
-    } // make sure property exists
-	} // go through each object
-} // go through each array element
+request = new XMLHttpRequest();
 
-update = document.querySelector('.links ul');
-update.innerHTML = output.join('');
+request.open('GET', 'js/data.json');
+request.onreadystatechange = function() {
+
+if ((request.status === 200) &&
+	(request.readyState === 4)) {
+
+			info = JSON.parse(request.responseText);
+
+			for (var i = 0; i <= info.links.length-1; i++) {
+				for (var key in info.links[i]) {
+					if (info.links[i].hasOwnProperty(key)) {
+						output.push('<li id="' + key + '">'); 
+						output.push('<a href="' + info.links[i][key]); 
+						output.push('">' + key + '</a>');
+						output.push('</li>');
+			    } // make sure property exists
+				} // go through each object
+			} // go through each array element
+
+
+		update = document.querySelector('.links ul');
+		update.innerHTML = output.join('');
+
+	} //ready
+}; //event
+request.send();
 
 }()); //self executing function call
