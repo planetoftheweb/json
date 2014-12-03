f7c4d3f1ebb9885928c2d23370ca9ef3d4d64025
Ray Villalobos <ray@planetoftheweb.com>
2014-12-03 13:24:52 -0500

    improved jshinting

---
+++ builds/development/js/script.js
@@ -0,0 +1,30 @@
+(function () {
+
+"use strict";
+
+var output = [], update;
+var	info = {
+	"full_name" : "Ray Villalobos",
+	"title" : "Staff Author",
+	"links" : {
+    "blog"     : "http://iviewsource.com",
+    "facebook" : "http://facebook.com/iviewsource",
+		"youtube"  : "http://www.youtube.com/planetoftheweb",
+		"podcast"  : "http://feeds.feedburner.com/authoredcontent",
+		"twitter"  : "http://twitter.com/planetoftheweb" 
+	}
+};
+
+for (var key in	info.links) {
+	if (info.links.hasOwnProperty(key)) {
+		output.push('<li id="' + key + '">'); 
+		output.push('<a href="' + info.links[key]); 
+		output.push('">' + key + '</a>');
+		output.push('</li>');
+	}
+}
+
+update = document.querySelector('.links ul');
+update.innerHTML = output.join('');
+
+}()); //self executing function call
