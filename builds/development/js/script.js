8758d6ee199289877a891cf40e2a77ce25f36d8f
Ray Villalobos <ray@planetoftheweb.com>
2014-12-04 11:30:25 -0500

    formatting changes

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,30 +1,31 @@
-(function () {
+(function() {
 
-"use strict";
+  "use strict";
 
-var output = [], update;
-var	info = {
-	"full_name" : "Ray Villalobos",
-	"title" : "Staff Author",
-	"links" : {
-    "blog"     : "http://iviewsource.com",
-    "facebook" : "http://facebook.com/iviewsource",
-		"youtube"  : "http://www.youtube.com/planetoftheweb",
-		"podcast"  : "http://feeds.feedburner.com/authoredcontent",
-		"twitter"  : "http://twitter.com/planetoftheweb" 
-	}
-};
+  var output = [],
+    update;
+  var info = {
+    "full_name": "Ray Villalobos",
+    "title": "Staff Author",
+    "links": {
+      "blog": "http://iviewsource.com",
+      "facebook": "http://facebook.com/iviewsource",
+      "youtube": "http://www.youtube.com/planetoftheweb",
+      "podcast": "http://feeds.feedburner.com/authoredcontent",
+      "twitter": "http://twitter.com/planetoftheweb"
+    }
+  };
 
-for (var key in	info.links) {
-	if (info.links.hasOwnProperty(key)) {
-		output.push('<li id="' + key + '">'); 
-		output.push('<a href="' + info.links[key]); 
-		output.push('">' + key + '</a>');
-		output.push('</li>');
-  } // make sure property exists
-} // go through each object
+  for (var key in info.links) {
+    if (info.links.hasOwnProperty(key)) {
+      output.push('<li id="' + key + '">');
+      output.push('<a href="' + info.links[key]);
+      output.push('">' + key + '</a>');
+      output.push('</li>');
+    } // make sure property exists
+  } // go through each object
 
-update = document.querySelector('.links ul');
-update.innerHTML = output.join('');
+  update = document.querySelector('.links ul');
+  update.innerHTML = output.join('');
 
 }()); //self executing function call
