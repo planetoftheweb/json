8ab05d52ea756f6adbae2c23e6e1958099091e70
Ray Villalobos <ray@planetoftheweb.com>
2014-12-04 06:25:32 -0500

    beautify

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,20 +1,22 @@
 function dataHandler(info) {
 
-	"use strict";
-	var output = [], update;
-	
-	for (var i = 0; i <= info.links.length-1; i++) {
-		for (var key in info.links[i]) {
-			if (info.links[i].hasOwnProperty(key)) {
-				output.push('<li id="' + key + '">'); 
-				output.push('<a href="' + info.links[i][key]); 
-				output.push('">' + key + '</a>');
-				output.push('</li>');
-	    } // make sure property exists
-		} // go through each object
-	} // go through each array element
+    "use strict";
+
+    var output = [],
+      update;
+
+    for (var i = 0; i <= info.links.length - 1; i++) {
+      for (var key in info.links[i]) {
+        if (info.links[i].hasOwnProperty(key)) {
+          output.push('<li id="' + key + '">');
+          output.push('<a href="' + info.links[i][key]);
+          output.push('">' + key + '</a>');
+          output.push('</li>');
+        } // make sure property exists
+      } // go through each object
+    } // go through each array element
 
 
-	update = document.querySelector('.links ul');
-	update.innerHTML = output.join('');
-} //dataHandler
+    update = document.querySelector('.links ul');
+    update.innerHTML = output.join('');
+  } //dataHandler
