a7f6a5344b646e721ae4087a2d41a425427ef720
Ray Villalobos <ray@planetoftheweb.com>
2014-12-03 19:38:12 -0500

    03_02 JSONP

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,37 +1,20 @@
-(function () {
+function dataHandler(info) {
 
-"use strict";
-
-var output = [], update, info;
-var request;
-
-request = new XMLHttpRequest();
-
-request.open('GET', 'js/data.json');
-request.onreadystatechange = function() {
-
-if ((request.status === 200) &&
-	(request.readyState === 4)) {
-
-			info = JSON.parse(request.responseText);
-
-			for (var i = 0; i <= info.links.length-1; i++) {
-				for (var key in info.links[i]) {
-					if (info.links[i].hasOwnProperty(key)) {
-						output.push('<li id="' + key + '">'); 
-						output.push('<a href="' + info.links[i][key]); 
-						output.push('">' + key + '</a>');
-						output.push('</li>');
-			    } // make sure property exists
-				} // go through each object
-			} // go through each array element
+	"use strict";
+	var output = [], update;
+	
+	for (var i = 0; i <= info.links.length-1; i++) {
+		for (var key in info.links[i]) {
+			if (info.links[i].hasOwnProperty(key)) {
+				output.push('<li id="' + key + '">'); 
+				output.push('<a href="' + info.links[i][key]); 
+				output.push('">' + key + '</a>');
+				output.push('</li>');
+	    } // make sure property exists
+		} // go through each object
+	} // go through each array element
 
 
-		update = document.querySelector('.links ul');
-		update.innerHTML = output.join('');
-
-	} //ready
-}; //event
-request.send();
-
-}()); //self executing function call
+	update = document.querySelector('.links ul');
+	update.innerHTML = output.join('');
+} //dataHandler
