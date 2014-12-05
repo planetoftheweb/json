1800ac3ce8c1b04da0e3a572c84c0cc99daba1ae
Ray Villalobos <ray@planetoftheweb.com>
2014-12-05 06:31:02 -0500

    04_02

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -2,5 +2,11 @@ $(function() {
 
   "use strict";
 
+  $.getJSON('js/data.json', function(data) {
+    var template = $('#speakerstpl').html();
+    var html = Mustache.to_html(template, data);
+    $('.carousel').html(html);
+
+  }); //getJSON
 
 }); //function
