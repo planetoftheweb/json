198240b6be6d2f68f3a156f64d8b22310bdfaf28
Ray Villalobos <ray@planetoftheweb.com>
2015-06-12 03:33:32 -0700

    modified to pure javascript without carousel

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,14 +1,15 @@
-$(function() {
   "use strict";
-  $.getJSON('js/data.json', function(data) {
-    var template = $('#speakerstpl').html();
-    var html = Mustache.to_html(template, data);
-
-    $('.carousel').html(html);
-    $('.carousel').slick({
-      autoplay: true,
-      arrows: true,
-      dots: true
-    }); // Slick
-  }); //getJSON
-}); //function
+  var request;
+  request=new XMLHttpRequest();
+  request.open('GET', 'js/data.json');
+  request.onreadystatechange = function() {
+    if ((request.status === 200) &&
+      (request.readyState === 4)) {
+        var data = JSON.parse(request.responseText);
+        console.log(data);
+        var template = document.querySelector('#speakerstpl').innerHTML;
+        var html = Mustache.to_html(template, data);
+        document.querySelector('#speakers').innerHTML=html;
+    } //ready
+  }; //event
+  request.send();
